c202c5c3aa25ba7a13cd000667639c833ab7e14f
Amir Omidfar <omidfar73@gmail.com>
2024-09-29 01:46:28 -0700

    git the mqtt and js vis code working with dummy generated data

--- MQTT/MosquittoBroker/mqtt_pub.py
+++ MQTT/MosquittoBroker/mqtt_pub.py
@@ -1,14 +1,24 @@
 import sys
-
+import time
+import random
 import paho.mqtt.client as paho
 
-
+port = 1883
 
 client = paho.Client(paho.CallbackAPIVersion.VERSION2)
 
-if client.connect("localhost", 1883, 60) != 0:
-    print("Couldn't connect to the mqtt broker")
-    sys.exit(1)
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    if client.connect("localhost", port, 60) != 0:
+        print("Couldn't connect to the mqtt broker")
+        sys.exit(1)
 
-client.publish("test_topic", "Hi, paho mqtt client works fine!", 0)
-client.disconnect()
+    client.publish("test_topic", "Hi, paho mqtt client works fine!", 0)
+    for i in range(100):
+        x,y,z = int(random.random()*1000),int(random.random()*100),int(random.random()*100)
+        coords_example = [x,y,z,0,0,0]
+        coords_string = ' '.join(str(x) for x in coords_example)
+        client.publish("test_topic",coords_string,0)
+        time.sleep(1)
+        #client.loop()
+    client.disconnect()
--- MQTT/MosquittoBroker/mqtt_sub.py
+++ MQTT/MosquittoBroker/mqtt_sub.py
@@ -2,25 +2,27 @@ import sys
 
 import paho.mqtt.client as paho
 
+port = 1883
 
 def message_handling(client, userdata, msg):
     print(f"{msg.topic}: {msg.payload.decode()}")
 
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    client = paho.Client(paho.CallbackAPIVersion.VERSION2)
+    client.on_message = message_handling
 
-client = paho.Client(paho.CallbackAPIVersion.VERSION2)
-client.on_message = message_handling
+    if client.connect("localhost", port, 60) != 0:
+        print("Couldn't connect to the mqtt broker")
+        sys.exit(1)
 
-if client.connect("localhost", 1883, 60) != 0:
-    print("Couldn't connect to the mqtt broker")
-    sys.exit(1)
+    client.subscribe("test_topic")
 
-client.subscribe("test_topic")
-
-try:
-    print("Press CTRL+C to exit...")
-    client.loop_forever()
-except Exception:
-    print("Caught an Exception, something went wrong...")
-finally:
-    print("Disconnecting from the MQTT broker")
-    client.disconnect()
+    try:
+        print("Press CTRL+C to exit...")
+        client.loop_forever()
+    except Exception:
+        print("Caught an Exception, something went wrong...")
+    finally:
+        print("Disconnecting from the MQTT broker")
+        client.disconnect()
